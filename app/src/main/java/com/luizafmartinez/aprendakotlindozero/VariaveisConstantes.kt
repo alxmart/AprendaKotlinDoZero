914134f52453651b510b866d6da4e6df4a05dc05
alxmart <alxmartinez@gmail.com>
2023-03-15 18:56:12 -0300

    Exercícios com Variáveis e constantes.

---
+++ app/src/main/java/com/luizafmartinez/aprendakotlindozero/VariaveisConstantes.kt
@@ -0,0 +1,27 @@
+package com.luizafmartinez.aprendakotlindozero
+
+fun retornaNomeUsuario(){
+    //Executa e retorna algo
+}
+
+const val nomeUsuario = "Homer"//tempo de compilacao
+
+class Usuario {
+
+}
+
+fun main() {
+
+    val nome = retornaNomeUsuario()//tempo de execução
+
+    /*
+    val numero1 = 20
+    val numero2 = 30
+    var resultado = numero1 + numero2
+  */
+    val numero = 20
+    val usuario = Usuario()
+
+    println(nomeUsuario)
+
+}
