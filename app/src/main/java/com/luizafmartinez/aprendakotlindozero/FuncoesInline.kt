b8183c28f790dd526c6c7d04cb990beb3fd4bd1c
alxmart <alxmartinez@gmail.com>
2023-03-19 16:10:23 -0300

    Exercícios / funções Inline.

--- app/src/main/java/com/luizafmartinez/aprendakotlindozero/FuncoesInline.kt
+++ app/src/main/java/com/luizafmartinez/aprendakotlindozero/FuncoesInline.kt
@@ -2,11 +2,14 @@ package com.luizafmartinez.aprendakotlindozero
 
 //Inline Function
 
-//fun subtrair() = println( 10 - 8 )
+fun subtrair() = println( 10 - 8 )
 
-fun subtrair():Int = 10-8
+//fun subtrair():Int = 10-8
 
 fun main() {
-    val retorno = subtrair()
-    println( retorno )
+
+    subtrair()
+
+    //val retorno = subtrair()
+    //println( retorno )
 }
