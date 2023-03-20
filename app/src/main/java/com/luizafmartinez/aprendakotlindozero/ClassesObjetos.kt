b5f75a6200b0ace116e4fb92e14181ab08a7df06
alxmart <alxmartinez@gmail.com>
2023-03-20 11:45:36 -0300

    Exercícios / Classes e objetos.

---
+++ app/src/main/java/com/luizafmartinez/aprendakotlindozero/ClassesObjetos.kt
@@ -0,0 +1,36 @@
+//Abstração
+// Pessoa: cpf, nome, endereço,
+// Cachorro: nome, raça, peso, porte,
+
+package com.luizafmartinez.aprendakotlindozero
+
+class Jogador {
+    var kart = ""
+    var pneu = ""
+    var planador = ""
+
+    fun acelerar(aceleracao:Int = 0) {
+        println("Acelerar na velocidade $aceleracao")
+    }
+    fun retornarPoder(): String {
+        return "Casco vermelho"
+    }
+    //  Funcao inline:
+    // ----------------------------------------------------
+    // fun retornarPoder(): String = "Casco Vermelho"
+
+ }
+
+fun main() {
+
+    val jogador = Jogador()
+    //jogador.kart = "Kart do Mario"
+    //jogador.pneu = ""
+    //jogador.planador = ""
+    jogador.acelerar( aceleracao = 80 )
+    val poder = jogador.retornarPoder()
+    println(poder)
+    //println(jogador.kart)
+
+}
+
