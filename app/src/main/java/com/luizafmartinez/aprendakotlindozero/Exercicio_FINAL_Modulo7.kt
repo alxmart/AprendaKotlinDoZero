20cab8a894132ba78d81f611778cd4cd814b78d5
alxmart <alxmartinez@gmail.com>
2023-03-23 20:05:11 -0300

    Exercicio FINAL, partes 1,2, 3.

--- app/src/main/java/com/luizafmartinez/aprendakotlindozero/Exercicio_FINAL_Modulo7.kt
+++ app/src/main/java/com/luizafmartinez/aprendakotlindozero/Exercicio_FINAL_Modulo7.kt
@@ -1,11 +1,18 @@
 package com.luizafmartinez.aprendakotlindozero
 
+interface Eletrificado {
+
+    fun motorEletrico()
+}
+
 abstract class Veiculo (
     nome: String = "",
     qtdRodas: Int = 0
     ) {
 
     abstract fun acelerar()
+
+    //abstract fun Eletrificado()
 }
 
 class Carro3(nome: String, qtdRodas: Int) : Veiculo() {
@@ -15,34 +22,38 @@ class Carro3(nome: String, qtdRodas: Int) : Veiculo() {
 
     override fun acelerar() {
 
-        println("Acelerar $nome com $qtdRodas rodas")
+        println("Acelerar $nome com $qtdRodas rodas.")
     }
 }
 
-class Moto(nome: String, qtdRodas: Int) : Veiculo() {
+class Moto(nome: String, qtdRodas: Int) : Veiculo(), Eletrificado {
 
     var nome: String = nome
     var qtdRodas: Int = qtdRodas
 
+    override fun motorEletrico() {
+        println("Rodando com motor elétrico.")
+    }
+
     override fun acelerar() {
 
-        println("Acelerar $nome com $qtdRodas rodas")
+        println("Acelerar $nome com $qtdRodas rodas.")
+        motorEletrico()
     }
+
+
 }
 
 fun main() {
 
-    val carrao = Carro3("Fusca",4)
-    println("Nome: ${carrao.nome}")
-    println("Quantidade de Rodas: ${carrao.qtdRodas}")
+    val carrao = Carro3("Lincoln Navigator",4)
+    //println("Nome: ${carrao.nome}")
+    //println("Quantidade de Rodas: ${carrao.qtdRodas}")
     carrao.acelerar()
-    println()
 
-    val motao = Moto("Yamaha",2)
-    println("Nome: ${motao.nome}")
-    println("Quantidade de Rodas: ${motao.qtdRodas}")
+    val motao = Moto("Harley Davidson Ultra Limited",2)
+    //println("Nome: ${motao.nome}")
+    //println("Quantidade de Rodas: ${motao.qtdRodas}")
     motao.acelerar()
-
-
 
 }
