d43cdb32f21c6e4063ab598db3d6fe860a683dba
alxmart <alxmartinez@gmail.com>
2023-03-23 17:39:14 -0300

    Exercícios  com  lateinit.

--- app/src/main/java/com/luizafmartinez/aprendakotlindozero/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/aprendakotlindozero/MainActivity.kt
@@ -2,10 +2,24 @@ package com.luizafmartinez.aprendakotlindozero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var texto: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        texto = findViewById(R.id.textNome)
+
+        texto.setText("inicial")
     }
+
+    fun alterarTexto() {
+        texto.setText("Alterado")
+    }
+
 }
