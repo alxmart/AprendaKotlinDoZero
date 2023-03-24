3c350a2735aaa777e6164977912b2b843c94974b
alxmart <alxmartinez@gmail.com>
2023-03-24 14:05:57 -0300

    Exercicio FINAL, partes 1 a 6.

--- app/src/main/java/com/luizafmartinez/aprendakotlindozero/Exercicio_FINAL_Modulo7.kt
+++ app/src/main/java/com/luizafmartinez/aprendakotlindozero/Exercicio_FINAL_Modulo7.kt
@@ -1,13 +1,24 @@
 package com.luizafmartinez.aprendakotlindozero
 
+import com.luizafmartinez.aprendakotlindozero.Carro3.Companion.exibeMensagemVelocidadeMaximaLei
+
+enum class StatusVeiculo {
+    FUNCIONAMENTO,
+    MANUTENCAO,
+    QUEBRADO
+}
+
 interface Eletrificado {
 
-    fun motorEletrico()
+    fun motorEletrico() {
+        println("Rodando com motor elétrico.")
+    }
 }
 
 abstract class Veiculo (
     nome: String = "",
-    qtdRodas: Int = 0
+    qtdRodas: Int = 0,
+    status: String = ""
     ) {
 
     abstract fun acelerar()
@@ -16,52 +27,78 @@ abstract class Veiculo (
         println("Intensidade da aceleração: $intensidade")
     }
 
+    abstract fun recuperarStatus()
 }
 
-class Carro3(nome: String, qtdRodas: Int) : Veiculo() {
+class Carro3(
+    nome: String,
+    qtdRodas: Int,
+    statusVeiculo: StatusVeiculo = StatusVeiculo.FUNCIONAMENTO
+) : Veiculo() {
 
     var nome: String = nome
     var qtdRodas: Int = qtdRodas
+    var status: StatusVeiculo = statusVeiculo
 
     override fun acelerar() {
-
         println("Acelerar $nome com $qtdRodas rodas.")
-        acelerar(2)
+        acelerar(3)
+    }
+    override fun recuperarStatus()  {
+        status = StatusVeiculo.MANUTENCAO
+        println("O status do veículo é: $status")
     }
 
+    companion object {
+
+        fun exibeMensagemVelocidadeMaximaLei(velmax: Int) {
+            println("Velocidade Máxima permitida por Lei é de: $velmax Km/h")
+        }
+    }
 
 }
 
-class Moto(nome: String, qtdRodas: Int) : Veiculo(), Eletrificado {
+class Moto(
+    nome: String,
+    qtdRodas: Int,
+    statusVeiculo: StatusVeiculo = StatusVeiculo.FUNCIONAMENTO
+) : Veiculo(), Eletrificado {
 
     var nome: String = nome
     var qtdRodas: Int = qtdRodas
-
-    override fun motorEletrico() {
-        println("Rodando com motor elétrico.")
-    }
+    var status: StatusVeiculo = statusVeiculo
 
     override fun acelerar() {
-
         println("Acelerar $nome com $qtdRodas rodas.")
         motorEletrico()
-        acelerar(4)
+        acelerar(5)
+    }
+
+    override fun recuperarStatus()  {
+        status = StatusVeiculo.MANUTENCAO
+        println("O status do veículo é: $status")
+    }
+
+    companion object {
+
+        fun exibeMensagemVelocidadeMaximaLei(velmax: Int) {
+            println("Velocidade Máxima permitida por Lei é de: $velmax Km/h")
+        }
     }
 
 }
 
 fun main() {
 
+    Carro3.exibeMensagemVelocidadeMaximaLei(120)
     val carrao = Carro3("Lincoln Navigator",4)
-    //println("Nome: ${carrao.nome}")
-    //println("Quantidade de Rodas: ${carrao.qtdRodas}")
     carrao.acelerar()
-    //carrao.acelerar(3)
+    carrao.recuperarStatus()
+    println("--------------------------------------------------")
 
-    val motao = Moto("Harley Davidson Ultra Limited",2)
-    //println("Nome: ${motao.nome}")
-    //println("Quantidade de Rodas: ${motao.qtdRodas}")
+    Moto.exibeMensagemVelocidadeMaximaLei(120)
+    val motao = Moto("Harley Davidson LiveWire ONE",2)
     motao.acelerar()
-    //motao.acelerar(4)
+    motao.recuperarStatus()
 
 }
