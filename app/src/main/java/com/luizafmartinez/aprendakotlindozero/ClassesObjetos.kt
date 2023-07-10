329d8be83551871725c7ed2b07fe13cefc3f34c6
alxmart <alxmartinez@gmail.com>
2023-07-10 11:28:22 -0300

    More tests and examples.

--- app/src/main/java/com/luizafmartinez/aprendakotlindozero/ClassesObjetos.kt
+++ app/src/main/java/com/luizafmartinez/aprendakotlindozero/ClassesObjetos.kt
@@ -8,7 +8,6 @@ class Jogador {
     var kart = ""
     var pneu = ""
     var planador = ""
-
     fun acelerar(aceleracao:Int = 0) {
         println("Acelerar na velocidade $aceleracao")
     }
@@ -18,19 +17,19 @@ class Jogador {
     //  Funcao inline:
     // ----------------------------------------------------
     // fun retornarPoder(): String = "Casco Vermelho"
-
  }
 
 fun main() {
-
     val jogador = Jogador()
+
     //jogador.kart = "Kart do Mario"
     //jogador.pneu = ""
     //jogador.planador = ""
+
     jogador.acelerar( aceleracao = 80 )
+
     val poder = jogador.retornarPoder()
     println(poder)
     //println(jogador.kart)
-
 }
 
